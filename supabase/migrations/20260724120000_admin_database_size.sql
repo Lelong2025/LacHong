60046ebf8b1810e12aced241214ff1e4f4bde039
Tạ Trần Vinh Quang <phuonglong@lhu.edu.vn>
2026-07-24 12:09:12 +0700

    commit lại bằng mail thầy để deploy netlify : )

--- supabase/migrations/20260724120000_admin_database_size.sql
+++ supabase/migrations/20260724120000_admin_database_size.sql
@@ -13,3 +13,4 @@ revoke all on function public.admin_database_size_bytes() from public;
 revoke all on function public.admin_database_size_bytes() from anon;
 revoke all on function public.admin_database_size_bytes() from authenticated;
 grant execute on function public.admin_database_size_bytes() to service_role;
+ 
